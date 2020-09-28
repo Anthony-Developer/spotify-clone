07111ec3de0ae543641aa7b0a9c6546e468f7f57
Anthony-Developer <AnthonyJimenez809@Gmail.com>
2020-09-27 22:02:49 -0400

    Created a Login Component

---
+++ src/Components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Styling/Login.css'
+
+function Login() {
+
+    return (
+        <div className="login-div">
+            <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
+            <a href="">Login with Spotify</a>
+        </div>
+    )
+}
+
+export default Login
