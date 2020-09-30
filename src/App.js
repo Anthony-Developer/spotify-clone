f7bc3a5b553239c64f0704d828e062ffc7eb39fd
Anthony-Developer <AnthonyJimenez809@Gmail.com>
2020-09-30 14:12:18 -0400

    Created a Data Layer using React Context API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
 import Login from './Components/Login'
+import Player from './Components/Player'
 import { getTokenFromUrl } from './Spotify'
+import SpotifyWebApi from 'spotify-web-api-js'
+
+const spotify = new SpotifyWebApi()
 
 function App() {
   const [token, setToken] = useState(null)
 
+  // On refresh we check if we have a token from Spotify
   useEffect(() => {
     const hash = getTokenFromUrl()
     window.location.hash = ""
@@ -13,15 +18,18 @@ function App() {
 
     if (_token) {
       setToken(_token)
+      spotify.setAccessToken(_token)
+      // Getting our user profile from Spotify
+      spotify.getMe().then(user => {
+        console.log('I am ', user)
+      })
     }
-
-    console.log('I have a token', _token)
   }, [])
 
   return (
     <div className="App">
       
-      <Login />
+      {token ? <Player /> : <Login />}
     
     </div>
   )
