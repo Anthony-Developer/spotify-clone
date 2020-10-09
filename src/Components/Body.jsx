1426e28b52d1b9b6f6bd93a97e62d7f5ff91b584
Anthony-Developer <AnthonyJimenez809@Gmail.com>
2020-10-09 18:53:02 -0400

    Styled the Header Component

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import '../Styling/Body.css'
+import Header from '../Components/Header'
 
-function Body() {
+function Body({spotify}) {
     return (
         <div className="body-div">
+            <Header spotify={spotify}/>
             <h1>Body Component</h1>
         </div>
     )
